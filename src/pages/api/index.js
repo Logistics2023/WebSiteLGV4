3d4339abd58a52362ddd9a182349ba75b2ac4e4f
Your Name <you@example.com>
2024-09-10 18:15:57 -0400

    DC

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,7 +1,16 @@
 const nodemailer = require('nodemailer');
-
+export const config = {
+    api: {
+        bodyParser: {
+            sizeLimit: '100mb',
+        },
+        responseLimit: '100mb',
+    }
+}
 export default function handler(req, res) {
-    console.log(req.body['Correo electrónico'])
+    console.log('------------------------------------------')
+
+    console.log(req.body)
     const transporter = nodemailer.createTransport({
         // host: 'mail.logisticsgear.com.bo',
         host: 'mail.logisticsappbolivia.com',
@@ -15,60 +24,90 @@ export default function handler(req, res) {
         }
     });
     async function handlerSendEmail() {
-
-        var fileBuffer = Buffer.from(req.body.cv, 'base64')
-        const response = await transporter.sendMail({
-            from: 'rrhh@logisticsappbolivia.com',
-            to: req.body['Correo electrónico'],
-            subject: 'Gracias por postular en Logistics Gear',
-            text: 'Gracias por postular, pronto nos comunicaremos con usted.',
-            html: '<p>Gracias por postular, pronto nos comunicaremos con usted.</p>'
-        });
-        await transporter.sendMail({
-            from: req.body['Correo electrónico'],
-            to: 'rrhh@logisticsappbolivia.com',
-            subject: ` Nueva postulación: ${req.body['Nombre completo']} - ${req.body['Area a postula']}`,
-            text: `
+        try {
+            // var fileBuffer = Buffer.from(req.body.cv, 'base64')
+            const response = await transporter.sendMail({
+                from: 'rrhh@logisticsappbolivia.com',
+                to: req.body['Correo electrónico'],
+                subject: 'Gracias por postular en Logistics Gear',
+                text: 'Gracias por postular, pronto nos comunicaremos con usted.',
+                html: '<p>Gracias por postular, pronto nos comunicaremos con usted.</p>'
+            });
+            const result = await transporter.sendMail({
+                from: req.body['Correo electrónico'],
+                to: 'rrhh@logisticsappbolivia.com',
+                subject: ` Nueva postulación: ${req.body['Nombre completo']} - ${req.body['Área a postula']}`,
+                text: `
                 Nombre: ${req.body['Nombre completo']}\n
                 Email: ${req.body['Correo electrónico']}\n
-                Área: ${req.body['Area a postula']}\n
+                Área: ${req.body['Área a postula']}\n
                 Celular: ${req.body['Celular']}\n
                 Residencia: ${req.body['Lugar de residencia']}\n
                 Tipo de postulación: ${req.body['Tipo de postulación']}\n
-                Estudia actualmente: ${req.body['']}\n
-                Institución: ${req.body}`,
-            attachments: [
-                {
-                    filename: `${req.body['Nombre completo']}.pdf`,
-                    content: req.body.cv.split("base64,")[1],
-                    encoding: 'base64'
-                }
-            ]
-        });
-        res.json({ success: 'true' })
+                ${req.body['Tipo de postulación'] && req.body['Tipo de postulación'] === 'Prácticas profesionales' ? `
+                    Estudia actualmente: ${req.body['Estudia actualmente']? 'Si' :'No'}\n
+                    Institucion educativa: ${req.body['Institucion educativa']}\n
+                    `: ''}
+                `,
+                attachments: [
+                    {
+                        filename: `${req.body['Nombre completo']}.pdf`,
+                        content: req.body.cv.split("base64,")[1],
+                        encoding: 'base64'
+                    }
+                ]
+            });
+
+            res.json({ success: 'true' })
+
+
+        } catch (err) {
+            res.json({ success: false, err })
+        }
+
     }
-
     handlerSendEmail()
-
-
-    // let transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: req.body['Correo electrónico'],
-    //         pass: 'hfyn zmif dudw lihd'
-    //     }
-    // });
-    // let mailOptions = {
-    //     from: 'votre.email@gmail.com',
-    //     to: req.body['Correo electrónico'],
-    //     subject: 'Envoi d\'email via Node.js',
-    //     text: 'Bonjour, ceci est un email envoyé via Node.js et Nodemailer.'
-    // };
-    // transporter.sendMail(mailOptions, function (error, info) {
-    //     if (error) {
-    //         console.log(error);
-    //     } else {
-    //         console.log('Email envoyé: ' + info.response);
-    //     }
-    // });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: req.body['Correo electrónico'],
+//         pass: 'hfyn zmif dudw lihd'
+//     }
+// });
+// let mailOptions = {
+//     from: 'votre.email@gmail.com',
+//     to: req.body['Correo electrónico'],
+//     subject: 'Envoi d\'email via Node.js',
+//     text: 'Bonjour, ceci est un email envoyé via Node.js et Nodemailer.'
+// };
+// transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email envoyé: ' + info.response);
+//     }
+// });
